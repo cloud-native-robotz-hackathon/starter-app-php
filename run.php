fd23503edc789a246a5df05cf813d0c8b5e754c6
Dennis Deitermann <Tuxos@users.noreply.github.com>
2019-04-04 01:57:47 +0200

    Update run.php

--- run.php
+++ run.php
@@ -16,11 +16,12 @@
 
 // Start coding here
 
-header ('Content-Type: image/png');
-$im = @imagecreatetruecolor(120, 20)
-      or die('Cannot Initialize new GD image stream');
+header("Content-type: image/png");
+$im = @imagecreate(50, 100)
+      or die("Kann keinen neuen GD-Bild-Stream erzeugen");
+$background_color = imagecolorallocate($im, 0, 0, 0);
 $text_color = imagecolorallocate($im, 233, 14, 91);
-imagestring($im, 1, 5, 5,  'A Simple Text String', $text_color);
+imagestring($im, 1, 5, 5, "Ein Test-String", $text_color);
 imagepng($im);
 imagedestroy($im);
 
