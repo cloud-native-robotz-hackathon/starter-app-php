d47c539cb913c62b360cf9490ca17b7da2bfdfbb
Dennis Deitermann <Tuxos@users.noreply.github.com>
2019-06-19 00:22:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <?php
    include("config.php");
-   echo '<title>Team '.$apikey.' - PHP Robocontrol</title>';
+   $team = strtoupper($apikey);
+   echo '<title>TEAM '.$team.' - PHP ROBOCONTROL</title>';
 ?>
 </head>
 <center>
