3253768847a60faa7f8241f9737a5fadd7ecfeb7
Dennis Deitermann <Tuxos@users.noreply.github.com>
2019-06-18 17:44:06 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-  $apikey = "b80392da7738d47d6ca7b35b62192e29"; // Key from 3Scale e.g. "b80392da7738d47d6ca7b35b62192e29"
+  $apikey = "r2d2"; // Key from 3Scale e.g. "marvin"
   $url = "https://api-2445582274375.staging.gw.apicast.io:443/api/robot"; // URL from 3Scale without / at the end. e.g. "https://api-2445582274375.staging.gw.apicast.io:443/api/robot"
  
 ?>
