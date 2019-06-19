fd8594a9ba0e2ba70d47e60691e1d530334e8a7f
Dennis Deitermann <Tuxos@users.noreply.github.com>
2019-06-19 15:10:36 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-  $apikey = "r2d2"; // Key from 3Scale e.g. "marvin"
+  $apikey = "Your API Key from 3Scale"; // Key from 3Scale e.g. "r2d2"
   $url = "https://api-2445582274375.staging.gw.apicast.io:443/api/robot"; // URL from 3Scale without / at the end. e.g. "https://api-2445582274375.staging.gw.apicast.io:443/api/robot"
  
 ?>
